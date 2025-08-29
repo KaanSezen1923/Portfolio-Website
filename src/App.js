5d4c9b128bcaab6dfd08dec5149be1af937cdb49
Kaan Sezen <119515258+KaanSezen1923@users.noreply.github.com>
2025-08-30 01:20:44 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,23 @@
+
+import './App.css';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
+import Projects from './components/Projects';
+import Articles from './components/Articles';
+import Contact from './components/Contact';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Home />
+      <About />
+      <Projects />
+      <Articles />
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
